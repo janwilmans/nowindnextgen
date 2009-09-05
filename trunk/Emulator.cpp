2a5ddbc1b8b2788fdbd87e49d603da606a213e0a
janwilmans <janwilmans@3214cde2-90de-11de-b045-6380ec9940d4>
2009-09-05 08:52:04 +0000

    first working implementation with wrapping emuTime range!

--- trunk/Emulator.cpp
+++ trunk/Emulator.cpp
@@ -22,15 +22,34 @@ void Emulator::initialize(void)
 {
     mScheduler = new Scheduler();
 
+    Uint32 a=1;
+    Uint32 b=3000000000;
+    Sint32 c = a - b;
+    printf("test c, should be -2999999999, but will fail: %d\n", c);
+
+    Uint64 d=1;
+    Uint64 e=3000000000;
+    Sint64 f = d - e;
+    printf("test f, should be -2999999999: %lld\n", f);
+
+
     DummyCpu* cpu = new DummyCpu();
 
 	mScheduler->addEvent(-1, MakeDelegate(this, &Emulator::interruptTestMethod));
 	mScheduler->addEvent(-5, MakeDelegate(this, &Emulator::interruptTestMethod));
     mScheduler->addEvent(-6, MakeDelegate(this, &Emulator::interruptTestMethod));
     mScheduler->addEvent(-14, MakeDelegate(this, &Emulator::interruptTestMethod));
-    mScheduler->addEvent(100, MakeDelegate(this, &Emulator::endOfRangeEvent));
+    mScheduler->addEvent(10, MakeDelegate(this, &Emulator::endOfRangeEvent));
 
     Emulator::emuTime = -20;
+    Emulator::emuTime++;
+    printf("Emulator::emuTime: %u\n", Emulator::emuTime);
+    Emulator::emuTime++;
+    printf("Emulator::emuTime: %u\n", Emulator::emuTime);
+    Emulator::emuTime++;
+
+    printf("Emulator::emuTime: %u\n", Emulator::emuTime);
+
     mScheduler->run(cpu);
     //mScheduler->runNice(cpu);
 }
