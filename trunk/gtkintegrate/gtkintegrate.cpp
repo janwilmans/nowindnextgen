03a45ed43f62122d41e21fe1556c8e702fbecd51
janwilmans <janwilmans@3214cde2-90de-11de-b045-6380ec9940d4>
2009-10-04 16:14:58 +0000

    a very QT Hello world

--- trunk/gtkintegrate/gtkintegrate.cpp
+++ trunk/gtkintegrate/gtkintegrate.cpp
@@ -1,17 +1,19 @@
 #include <gtk/gtk.h>
 
+int main_ex( int argc, char **argv );
+
 int main( int   argc,
           char *argv[] )
 {
-    GtkWidget *window;
+    return main_ex(argc, argv);
     
+    GtkWidget *window;
     gtk_init (&argc, &argv);
     
     window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
     gtk_widget_show  (window);
     
     gtk_main ();
-    
     return(0);
 }
 
