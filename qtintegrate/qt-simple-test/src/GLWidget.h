3fd2117ce9e7bc3ffaa40ef27f1af167cc87f641
janwilmans <janwilmans@3214cde2-90de-11de-b045-6380ec9940d4>
2009-10-08 20:26:40 +0000

    added GLWidget and new test executable

---
+++ qtintegrate/qt-simple-test/src/GLWidget.h
@@ -0,0 +1,19 @@
+#ifndef GLWIDGET_H
+#define GLWIDGET_H
+
+#include <QtOpenGL/QGLWidget>
+
+class GLWidget : public QGLWidget
+{
+    // meta object linker errors?
+    //Q_OBJECT 
+
+public:
+    GLWidget(QWidget *parent = 0);
+    ~GLWidget();
+    
+    virtual void paintEvent(QPaintEvent *event);
+
+};
+
+#endif // GLWIDGET_H
