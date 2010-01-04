28af8a3d5618438529e40d9e18a5918487393b79
janwilmans <janwilmans@3214cde2-90de-11de-b045-6380ec9940d4>
2010-01-04 20:51:06 +0000

    added cpu/CMakeLists.txt

--- debug.h
+++ debug.h
@@ -2,16 +2,18 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
+void nw_assert(const char* aFilename, int aLine, bool aAssertion, const char* aAssertionText);
 #ifdef _DEBUG
     #define NW_ASSERT(assertion) nw_assert(__FILE__, __LINE__, assertion, #assertion);
 #else
     #define NW_ASSERT(assertion) //
 #endif
 
+void nw_debug(const char *cFormat, ...);
 #define DBERR nw_debug
 
-void nw_debug(const char *cFormat, ...);
-void nw_assert(const char* aFilename, int aLine, bool aAssertion, const char* aAssertionText);
+void nw_logmsg(const char* aFilename, int aLine, const char *cFormat, ...);
+#define LOGMSG(...) nw_logmsg(__FILE__, __LINE__, __VA_ARGS__)
 
 #if __GNUC__ == 4
     #define likely(x)       __builtin_expect(!!(x), 1)
