64e8cc2a0b4fc1ce6b16f8d98d4774afe233a397
janwilmans <janwilmans@3214cde2-90de-11de-b045-6380ec9940d4>
2010-01-08 21:19:57 +0000

    fixed CR/LF and class variables names

--- trunk/ClockChip.cpp
+++ trunk/ClockChip.cpp
@@ -8,17 +8,17 @@ using namespace fastdelegate;
 ClockChip::ClockChip(Emulator& aEmulator) : BusComponent(aEmulator)
 {
 	DBERR("YYOOOOO clockChip in da house!!!!!!!\n");
-	timeDiff = 0;
+	mTimeDiff = 0;
 
-	modeRegister = 0;	// todo: verify initial values
-	testRegister = 0;
+	mModeRegister = 0;	// todo: verify initial values
+	mTestRegister = 0;
 	//resetRegister = 0; // todo: bijhouden?
 
 	for (int i=0;i<3;i++)
 	{
 		for (int j=0;j<13;j++)
 		{
-			dataRegister[j][i] = 0;
+			mDataRegister[j][i] = 0;
 		}
 	}
 }
@@ -59,18 +59,18 @@ byte ClockChip::readData(word /*port*/)
 	// mask unused bits (block0 and 1)
 	//DBERR("RTC: read[%u] (emuTime %d)\n", address, Emulator::emuTime);
 	//return 255;
-	switch(address)
+	switch(mAddress)
 	{
 	case 0x0d:
-		DBERR("RTC: read modeRegister[0x%02x]\n", modeRegister | 0xf0);
-		return modeRegister | 0xf0;
+		DBERR("RTC: read modeRegister[0x%02x]\n", mModeRegister | 0xf0);
+		return mModeRegister | 0xf0;
 
 	case 0x0e:
 	case 0x0f:
 		return 0xff;
 	}
 	
-	int block = modeRegister & 3;
+	int block = mModeRegister & 3;
 	if (block == 0)
 	{
 		// time block
@@ -80,10 +80,10 @@ byte ClockChip::readData(word /*port*/)
 		byte value;
 
 		localTime = time(NULL);
-		msxTime = localTime - timeDiff;
+		msxTime = localTime - mTimeDiff;
 		timeinfo = localtime(&msxTime);
 
-		switch (address)
+		switch (mAddress)
 		{
 		case 0x00:	value = timeinfo->tm_sec%10; break;
 		case 0x01:	value = timeinfo->tm_sec/10; break;
@@ -105,46 +105,46 @@ byte ClockChip::readData(word /*port*/)
 		return value | 0xf0;
 	}	
 
-	if ((block == 1) && (address = 0x0b)) {
+	if ((block == 1) && (mAddress = 0x0b)) {
 		// todo: update leap year (in alarm block)
 	}
-	DBERR("RTC: read register[%u][%u] value=0x%02x\n", address, block, dataRegister[address][block-1] | 0xf0);
-	return dataRegister[address][block-1] | 0xf0;
+	DBERR("RTC: read register[%u][%u] value=0x%02x\n", mAddress, block, mDataRegister[mAddress][block-1] | 0xf0);
+	return mDataRegister[mAddress][block-1] | 0xf0;
 }
 
 void ClockChip::writeAddress(word /*port*/, byte value)
 {
 	// set write and read address
-	address = value & 15;
+	mAddress = value & 15;
 }
 
 void ClockChip::writeData(word /*port*/, byte value)
 {
 	value &= 15;
 	
-	switch (address)
+	switch (mAddress)
 	{
 	case 0x0d:
 		// todo: acties?
 		DBERR("RTC: write modeRegister[0x%02x]\n", value);
-		modeRegister = value;
+		mModeRegister = value;
 		break;
 	case 0x0e:
 		DBERR("RTC: write testRegister[0x%02x]\n", value);
-		testRegister = value;
+		mTestRegister = value;
 		break;
 	case 0x0f:
 		DBERR("RTC: write resetRegister[0x%02x]\n", value);
 		if (value & 1)
 		{
 			// reset ALARM
-			for (int i=2;i<=8;i++) dataRegister[i][0] = 0;
+			for (int i=2;i<=8;i++) mDataRegister[i][0] = 0;
 		}
 		// todo: reset seconds?
 		break;
 	default:
-		DBERR("RTC: write register[0x%02x][%u] with 0x%02x\n", address, modeRegister&3, value);
-		dataRegister[address][(modeRegister & 3)-1] = value;
+		DBERR("RTC: write register[0x%02x][%u] with 0x%02x\n", mAddress, mModeRegister & 3, value);
+		mDataRegister[mAddress][(mModeRegister & 3)-1] = value;
 	}
 }
 
